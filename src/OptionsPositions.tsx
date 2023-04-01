e0873fc6ae824934e1469426e1eed35f1faa965a
Anant <techieanant@gmail.com>
2023-04-01 14:06:58 -0700

    Fix chart

--- src/OptionsPositions.tsx
+++ src/OptionsPositions.tsx
@@ -1,13 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import Dexie from 'dexie';
-import { fetchLongAndShortPositions } from './helpers';
-import { format } from 'date-fns';
+import React, { useEffect, useState } from "react";
+import {
+  AreaChart,
+  Area,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import Dexie from "dexie";
+import { fetchLongAndShortPositions } from "./helpers";
+import { format } from "date-fns";
 
 interface ChartData {
-    timestamp: number;
-    longAmount: number;
-    shortAmount: number;
+  timestamp: number;
+  longAmount: number;
+  shortAmount: number;
 }
 
 interface OptionsData {
@@ -17,35 +28,51 @@ interface OptionsData {
   shortAmount: number;
 }
 
-
 const OptionsChart = () => {
   const [chartData, setChartData] = useState<Array<ChartData>>([]);
-  const db = new Dexie('optionsData') as Dexie & { data: Dexie.Table<OptionsData, number> };
+  const db = new Dexie("optionsData") as Dexie & {
+    data: Dexie.Table<OptionsData, number>;
+  };
 
   db.version(1).stores({
-    data: '++id, timestamp, longAmount, shortAmount',
+    data: "++id, timestamp, longAmount, shortAmount",
   });
 
   const fetchData = async () => {
-    // Use the provided code to fetch long and short positions
-    const { availableLongAmountParsed, availableShortAmountParsed } = await fetchLongAndShortPositions();
-
-    // Save fetched data to IndexedDB with timestamp
-    await db.data.add({
-      timestamp: Date.now(),
-      longAmount: availableLongAmountParsed,
-      shortAmount: availableShortAmountParsed,
-    });
-
-    // Update chart data
-    setChartData((prevData) => [
-      ...prevData,
-      {
+    try {
+      const { availableLongAmountParsed, availableShortAmountParsed } =
+        await fetchLongAndShortPositions();
+      // Save fetched data to IndexedDB with timestamp
+      await db.data.add({
         timestamp: Date.now(),
         longAmount: availableLongAmountParsed,
         shortAmount: availableShortAmountParsed,
-      } as ChartData
-    ]);
+      });
+
+      // Update chart data
+      setChartData((prevData) => [
+        ...prevData,
+        {
+          timestamp: Date.now(),
+          longAmount: availableLongAmountParsed,
+          shortAmount: availableShortAmountParsed,
+        } as ChartData,
+      ]);
+    } catch {
+      console.error("Error Fetching Data");
+      // Use Stored Data
+      const chartData = await (
+        await db.data.toArray()
+      ).map(
+        (item) =>
+          ({
+            timestamp: item.timestamp,
+            longAmount: item.longAmount,
+            shortAmount: item.shortAmount,
+          } as ChartData)
+      );
+      setChartData(chartData);
+    }
   };
 
   useEffect(() => {
@@ -65,28 +92,44 @@ const OptionsChart = () => {
 
   // Format chart data
   const formattedData = chartData.map((item) => ({
-    timestamp: format(new Date(item.timestamp), 'MMM d, h:mm a'),
+    timestamp: format(new Date(item.timestamp), "MMM d, h:mm a"),
     longAmount: item.longAmount,
     shortAmount: item.shortAmount,
   }));
 
   return (
     // <ResponsiveContainer width={1000}>
-      <LineChart
-        width={1200}
-        height={700}
-        data={formattedData}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="timestamp" label={{ value: 'Timestamp', position: 'bottom' }} />
-        <YAxis label={{ value: 'Amount', angle: -90, position: 'bottom' }} />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="longAmount" stroke="green" activeDot={{ r: 8 }} />
-        <Line type="monotone" dataKey="shortAmount" stroke="red" />
-      </LineChart>
-    // </ResponsiveContainer>
+    <AreaChart
+      width={1800}
+      height={1000}
+      data={formattedData}
+      margin={{ top: 5, right: 20, left: 20, bottom: 5 }}
+      style={{ position: "absolute", top: 0, left: 0 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis
+        dataKey="timestamp"
+        label={{ value: "Timestamp", position: "bottom" }}
+      />
+      <YAxis label={{ value: "Amount", angle: -90, position: "bottom" }} />
+      <Tooltip />
+      <Legend />
+      <Area
+        type="monotone"
+        dataKey="longAmount"
+        fill="none"
+        stroke="green"
+        strokeWidth={8}
+      />
+      <Area
+        type="monotone"
+        dataKey="shortAmount"
+        fill="none"
+        stroke="red"
+        strokeWidth={8}
+      />
+    </AreaChart>
+    // </ResponsiveContainer
   );
 };
 
